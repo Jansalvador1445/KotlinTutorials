ca0a7a389b8256d1c3d227ca017bd2956c49084d
Jan Salvador Sebastian <jansalvadorsebastian@gmail.com>
2019-03-27 20:37:30 +0800

    Added Variable & Data types

---
+++ src/Tutorial 2 - Variable & DataTypes.kt
@@ -0,0 +1,27 @@
+fun main(args:Array<String>) {
+
+    //Numbers
+    println("Integer - " + 10)
+    println("Float - " + 10.0)
+
+    //Boolean
+    var isActive:Boolean
+    isActive = true
+    println("Boolean ${isActive}")
+
+    isActive = false
+    println("Boolean ${isActive}")
+
+    //String
+    var stringValue:String
+    stringValue = "Hello World"
+    println("String Value is ${stringValue}")
+
+    /**
+     * NOTE
+     * var - Mutable, it can change the value.
+     * val - Immutable, value can't be change.
+     * **/
+
+
+}
