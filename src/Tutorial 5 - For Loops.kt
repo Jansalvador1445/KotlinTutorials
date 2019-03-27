b68519f9966710e4f4331f3531306d5565c1bf9c
Jan Salvador Sebastian <jansalvadorsebastian@gmail.com>
2019-03-27 21:37:55 +0800

    Added Tutorial 5 - For Loops.kt

---
+++ src/Tutorial 5 - For Loops.kt
@@ -0,0 +1,52 @@
+fun main(args:Array<String>) {
+
+    //For Loops using the ranges (Tutorial 3 - Ranges.kt)
+    println("Method #1 : 1 - 5")
+    for (i in 1..5){
+        println("${i}")
+    }
+
+    //For Loops using the ranges (Tutorial 3 - Ranges.kt)
+    println("\nMethod #2: 5 - 1")
+    for (i in 5 downTo 1){
+        println("${i}")
+    }
+
+    //For Loops using the ranges (Tutorial 3 - Ranges.kt)
+    println("\nMethod #3: 1 3 5")
+    for (i in 1..5 step 2){
+        println("${i}")
+    }
+
+    //For Loops using the ranges (Tutorial 3 - Ranges.kt)
+    println("\nMethod #4: 5 3 1")
+    for (i in 5 downTo 1 step 2){
+        println("${i}")
+    }
+
+    //For Loops using the ranges (Tutorial 3 - Ranges.kt)
+    println("\nMethod #5: a - c")
+    for (i in 'a'..'c'){
+        println("${i}")
+    }
+
+    //For Loops using the ranges (Tutorial 3 - Ranges.kt)
+    println("\nMethod #5: c - a")
+    for (i in 'c' downTo 'a'){
+        println("${i}")
+    }
+
+    //For Loops using the ranges (Tutorial 3 - Ranges.kt)
+    println("\nMethod #6: a c e")
+    for (i in 'a' .. 'f' step 2){
+        println("${i}")
+    }
+
+    //For Loops using the ranges (Tutorial 3 - Ranges.kt)
+    println("\nMethod #7: f d b")
+    for (i in 'f' downTo 'a' step 2){
+        println("${i}")
+    }
+
+
+}
