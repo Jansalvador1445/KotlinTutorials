20b6edea12272af618ac2b57d28ee95588a60d12
Jan Salvador Sebastian <jansalvadorsebastian@gmail.com>
2019-03-28 16:07:27 +0800

    Added Tutorial 13 - Calling the Class from the oter file

---
+++ src/Tutorial 13 - Calling the Class from other file.kt
@@ -0,0 +1,5 @@
+fun main(args:Array<String>) {
+    var callingObject = callObj_Tutorial13("Hello World")
+    callingObject.display()
+}
+
---
+++ src/Tutorial 13_1 - Getting the Object.kt
@@ -0,0 +1,6 @@
+
+class callObj_Tutorial13(var stringValue: String ) {
+    fun display() {
+        print("String Value from Tutorial 13_1 is ${stringValue}")
+    }
+}
