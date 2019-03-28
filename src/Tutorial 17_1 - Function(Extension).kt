7ca7b7d03aa4b7721ef0f792b08dfb5159c022ba
Jan Salvador Sebastian <jansalvadorsebastian@gmail.com>
2019-03-28 17:41:14 +0800

    Added Tutorial 17_1 - Functions(Extension)

---
+++ src/Tutorial 17_1 - Function(Extension).kt
@@ -0,0 +1,23 @@
+import com.sun.org.apache.xpath.internal.operations.Bool
+
+fun main(args: Array<String>) {
+    var gradeValue:Int = 86
+    var cota = Grades()
+
+    println("Passed: ${cota.isPassed(gradeValue)}")
+    println("TES: ${cota.isChed(gradeValue)}")
+}
+
+
+class Grades{
+    fun isPassed(grade:Int) :Boolean{
+        return (grade > 50)
+    }
+}
+
+fun Grades.isChed(grade: Int) :Boolean{
+    return (grade > 85)
+}
+
+
+
